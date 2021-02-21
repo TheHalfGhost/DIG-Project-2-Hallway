1028d759ad7b18c33e61329efb30fc296608abde
TheHalfGhost <Alexander.THG@knights.ucf.edu>
2021-02-21 14:59:06 -0500

    first commit

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        
+        if (isPaused)
+        {           
+            ActivateMenu();
+        }
+        else
+        {   
+            DeacivateMenu();
+        }
+    }
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseMenuUI.SetActive(true);
+    }
+    public void DeacivateMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        isPaused = false;
+    }
+}
